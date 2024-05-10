683c934f5d6b1bccf0b1a5974ba102e1bdf62c96
Saalu Issaka <saaluissaka@gmail.com>
2024-05-10 17:43:35 +0000

    Toast for Edit_Delete

--- src/components/crud/Delete.js
+++ src/components/crud/Delete.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React,{useState}from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
-const Delete = () => {
+const Delete = ({ open, setOpen}) => {
+    const navigate = useNavigate()
+  
+        const onDelete = () => {
+            console.log('Check me out!')
+            navigate('/')
+  }
+
   return (
     <div>
       <div className='delete-container'>
-        <p className='msg'>Do you want to delete?</p>
-        <span>Yes</span>
-        <span>No</span>
+        <div className='card'>
+            <p className='msg'>Are sure you want to delete?</p>
+            <button  onClick={()=> setOpen(!open)}  className='btn cancel'>No</button>
+            <button onClick={() =>onDelete()} className='btn del'>Yes</button>
+        </div>
       </div>
     
     </div>
--- src/components/crud/Edit.js
+++ src/components/crud/Edit.js
@@ -2,7 +2,7 @@ import React,{useState,useEffect} from 'react'
 import {Link, useNavigate} from 'react-router-dom';
 
 
-const Edit = () => {
+const Edit = ({ showEdit, setShowEdit}) => {
     const navigate = useNavigate()
     const [data, setData] = useState({
         fullname: '' ,
@@ -11,9 +11,9 @@ const Edit = () => {
         salary: ''
     })
 
+
     const onSubmit = (e) =>{
         e.preventDefault()
-
 
         navigate('/dashboard')
 
@@ -27,25 +27,28 @@ const Edit = () => {
         //     dept: dept,
         //     salary: salary
         // })
-
-
     },[])
 
   return (
    
-    <div className='auth-container'>
-      <form onSubmit={onSubmit} className='' style={{width:'50rem'}}>
-            <h3>Update Employee Info</h3>
-            <span>Fullname</span>
-            <input  type=' ' placeholder='Joe Doe' />
-            <span>Position</span>
-            <input  type=' ' placeholder='Field Engineer' />
-            <span>Department</span>
-            <input  type=' ' placeholder='Managed Service' />
-            <span>Salary</span>
-            <input  type=' ' placeholder='20000' />
-            <input  type='submit' style={{background:'#25D366'}} className='btn' value='Save' placeholder='' />
-      </form>
+    <div>
+        <div className='auth-container edit-container '>
+
+            <form onSubmit={onSubmit} className='' style={{width:'50rem'}}>
+                    <p  onClick={()=> setShowEdit(!showEdit)}  className='close-editor'><span>X</span></p>
+                    <h3>Update Employee Info</h3>
+                    <span>Fullname</span>
+                    <input  type=' ' placeholder='Joe Doe' />
+                    <span>Position</span>
+                    <input  type=' ' placeholder='Field Engineer' />
+                    <span>Department</span>
+                    <input  type=' ' placeholder='Managed Service' />
+                    <span>Salary</span>
+                    <input  type=' ' placeholder='20000' />
+                    <input  type='submit' style={{background:'#25D366'}} className='btn' value='Save' 
+                    placeholder='' />
+            </form>
+        </div>
     </div>
   )
 }
